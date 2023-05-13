1d5df5d341378a83753667bbea3ef3cd9cc69c76
sitorasu <13363542+sitorasu@users.noreply.github.com>
2023-05-14 03:00:12 +0900

    ROMファイルのロード処理を更新

--- src/main.cc
+++ src/main.cc
@@ -14,19 +14,13 @@ using std::uint16_t;
 using std::uint32_t;
 using std::uint8_t;
 
-int main(int argc, char* argv[]) {
-  // コマンドラインの書式をチェック
-  if (argc < 2) {
-    std::cerr << "usage: gbemu ROM_FILE\n";
-    return 0;
-  }
+namespace {
 
-  // ROMファイルをオープン
-  std::string path = argv[1];
+std::vector<uint8_t> LoadRom(const std::string& path) {
   std::ifstream ifs(path, std::ios_base::in | std::ios_base::binary);
   if (ifs.fail()) {
     std::cerr << "File open error: " << path << "\n";
-    return 0;
+    std::exit(0);
   }
 
   // ROMファイルを読み出す
@@ -35,15 +29,30 @@ int main(int argc, char* argv[]) {
   std::vector<uint8_t> rom(it_ifs_begin, it_ifs_end);
   if (ifs.fail()) {
     std::cerr << "File read error: " << path << "\n";
-    return 0;
+    std::exit(0);
   }
 
   // ROMファイルをクローズ
   ifs.close();
   if (ifs.fail()) {
     std::cerr << "File close error: " << path << "\n";
+    std::exit(0);
+  }
+
+  return rom;
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  // コマンドラインの書式をチェック
+  if (argc < 2) {
+    std::cerr << "usage: gbemu ROM_FILE\n";
     return 0;
   }
+
+  // ROMファイルをロード
+  std::vector<uint8_t> rom(LoadRom(argv[1]));
 
   Cartridge cartridge(std::move(rom));
   cartridge.mbc().Read8(0);
