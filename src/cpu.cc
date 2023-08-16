10b66363d520eb36864c30573d6b958c47b1932c
sitorasu <13363542+sitorasu@users.noreply.github.com>
2023-08-16 15:04:18 +0900

    cpuのリファクタリング

--- src/cpu.cc
+++ src/cpu.cc
@@ -4,6 +4,7 @@
 #include <cstdio>
 #include <memory>
 #include <string>
+#include <utility>
 
 #include "command_line.h"
 #include "memory.h"
@@ -11,100 +12,95 @@
 
 namespace gbemu {
 
-namespace {
-class Nop : public Instruction {
- public:
-  Nop(std::uint16_t address) : Instruction(0, address) {}
-  std::string GetMnemonicString() override { return "nop"; }
-  unsigned length() override { return 1; }
-  unsigned cycles() override { return 4; }
-};
+std::string Cpu::Nop::GetMnemonicString() { return "nop"; }
 
-class JpU16 : public Instruction {
- public:
-  JpU16(std::uint32_t raw_code, std::uint16_t address, std::uint16_t imm)
-      : Instruction(raw_code, address), imm_(imm) {}
-  std::string GetMnemonicString() override {
-    char buf[16];
-    std::sprintf(buf, "jp %04X", imm_);
-    return std::string(buf);
-  }
-  unsigned length() override { return 3; }
-  unsigned cycles() override { return 16; }
-
- private:
-  std::uint16_t imm_;
-};
-
-}  // namespace
-
-std::shared_ptr<Instruction> Cpu::ExecuteNop() {
-  std::uint16_t pc = registers_.pc();
-  std::shared_ptr<Instruction> nop(new Nop(pc));
-  registers_.set_pc(pc + nop->length());
-  return nop;
+void Cpu::Nop::Execute(Cpu& cpu) {
+  std::uint16_t pc = cpu.registers_.pc();
+  cpu.registers_.set_pc(pc + length());
 }
 
-std::shared_ptr<Instruction> Cpu::ExecuteJpU16() {
-  std::uint16_t pc = registers_.pc();
-  std::uint8_t opcode = memory_.Read8(pc);
-  std::uint16_t imm = memory_.Read16(pc + 1);
-  std::uint32_t raw_code = ((std::uint32_t)imm << 8) | opcode;
-  registers_.set_pc(imm);
-  return std::shared_ptr<Instruction>(new JpU16(raw_code, pc, imm));
+std::string Cpu::JpU16::GetMnemonicString() {
+  char buf[16];
+  std::sprintf(buf, "jp 0x%04X", imm_);
+  return std::string(buf);
 }
 
-std::shared_ptr<Instruction> Cpu::ExecutePrefixedInstruction() {
+void Cpu::JpU16::Execute(Cpu& cpu) { cpu.registers_.set_pc(imm_); }
+
+std::shared_ptr<Cpu::Instruction> Cpu::FetchPrefixedInstruction() {
   std::uint16_t pc = registers_.pc();
   std::uint8_t opcode = memory_.Read8(pc + 1);
   UNREACHABLE("Unknown opcode: CB %02X", opcode);
 }
 
-std::shared_ptr<Instruction> Cpu::ExecuteInstruction() {
+std::shared_ptr<Cpu::Instruction> Cpu::FetchNop() {
+  std::uint16_t pc = registers_.pc();
+  return std::shared_ptr<Instruction>(new Nop(pc));
+}
+
+std::shared_ptr<Cpu::Instruction> Cpu::FetchJpU16() {
+  std::uint16_t pc = registers_.pc();
+  std::uint8_t opcode = memory_.Read8(pc);
+  std::uint16_t imm = memory_.Read16(pc + 1);
+  std::vector<std::uint8_t> raw_code{opcode,
+                                     static_cast<std::uint8_t>(imm & 0xFF),
+                                     static_cast<std::uint8_t>(imm >> 8)};
+  return std::shared_ptr<Instruction>(new JpU16(std::move(raw_code), pc, imm));
+}
+
+std::shared_ptr<Cpu::Instruction> Cpu::FetchInstruction() {
   std::uint16_t pc = registers_.pc();
   std::uint8_t opcode = memory_.Read8(pc);
   if (opcode == 0xCB) {
     // プレフィックスありの命令の実行
-    return ExecutePrefixedInstruction();
+    return FetchPrefixedInstruction();
   }
   if (opcode == 0x00) {
-    return ExecuteNop();
+    return FetchNop();
   }
   if (opcode == 0xC3) {
-    return ExecuteJpU16();
+    return FetchJpU16();
   }
   UNREACHABLE("Unknown opcode: %02X\n", opcode);
 }
 
 namespace {
-// 0xABCDEF -> "EFCDAB"
-std::string ConvertToLittleEndianString(std::uint32_t value) {
+// バイト列を文字列に変換する。
+// 例：{ 0xAB, 0xCD, 0xEF } -> "AB CD EF"
+std::string Join(const std::vector<std::uint8_t>& v) {
+  ASSERT(v.size() <= 4, "raw_code size is too large.");
   char buf[16];
   char* p = buf;
-  do {
-    sprintf(p, "%02X", value & 0xFF);
-    value >>= 8;
-    p += 2;
-  } while (value != 0);
-  return std::string(buf);
-}
-
-void PrintInstructionInfo(std::shared_ptr<Instruction> inst) {
-  char buf[64];
-  std::string raw_code = ConvertToLittleEndianString(inst->raw_code());
-  std::string mnemonic = inst->GetMnemonicString();
-  std::sprintf(buf, "$%04X %s\t%s", inst->address(), raw_code.c_str(),
-               mnemonic.c_str());
-  std::printf("%s\n", buf);
+  for (std::uint8_t num : v) {
+    sprintf(p, "%02X ", num);
+    p += 3;
+  }
+  std::string str{buf};
+  str.pop_back();
+  return str;
 }
 }  // namespace
 
 unsigned Cpu::Step() {
-  std::shared_ptr<Instruction> inst = ExecuteInstruction();
+  std::shared_ptr<Instruction> inst = FetchInstruction();
+  Execute(inst);
+
+  // デバッグモードなら命令の情報を表示
+  // 表示例
+  // $0100 00         nop
+  // $0101 C3 37 06   jp 0x0637
+  // $0637 C3 30 04   jp 0x0430
+  // ...
   if (options.debug()) {
-    PrintInstructionInfo(inst);
+    char buf[64];
+    std::string raw_code = Join(inst->raw_code());
+    std::string mnemonic = inst->GetMnemonicString();
+    std::sprintf(buf, "$%04X %s\t%s", inst->address(), raw_code.c_str(),
+                 mnemonic.c_str());
+    std::printf("%s\n", buf);
   }
-  return inst->cycles();
+
+  return inst->mcycles();
 }
 
 }  // namespace gbemu
--- src/cpu.h
+++ src/cpu.h
@@ -4,100 +4,12 @@
 #include <cstdint>
 #include <memory>
 #include <string>
+#include <utility>
+#include <vector>
 
 #include "memory.h"
 
 namespace gbemu {
-
-// CPUのレジスタを表すクラス。
-// Example:
-//     CpuRegisters regs;
-//     regs.set_pc(0x100);
-class CpuRegisters {
- public:
-  std::uint8_t a() { return a_; }
-  std::uint8_t b() { return b_; }
-  std::uint8_t c() { return c_; }
-  std::uint8_t d() { return d_; }
-  std::uint8_t e() { return e_; }
-  std::uint8_t h() { return h_; }
-  std::uint8_t l() { return l_; }
-  std::uint16_t af() { return ((std::uint16_t)a_ << 8) | flags_; }
-  std::uint16_t bc() { return ((std::uint16_t)b_ << 8) | c_; }
-  std::uint16_t de() { return ((std::uint16_t)d_ << 8) | e_; }
-  std::uint16_t hl() { return ((std::uint16_t)h_ << 8) | l_; }
-  std::uint16_t sp() { return sp_; }
-  std::uint16_t pc() { return pc_; }
-  bool z_flag() { return flags_ & (1 << 7); }
-  bool n_flag() { return flags_ & (1 << 6); }
-  bool h_flag() { return flags_ & (1 << 5); }
-  bool c_flag() { return flags_ & (1 << 4); }
-  void set_a(std::uint8_t value) { a_ = value; }
-  void set_b(std::uint8_t value) { b_ = value; }
-  void set_c(std::uint8_t value) { c_ = value; }
-  void set_d(std::uint8_t value) { d_ = value; }
-  void set_e(std::uint8_t value) { e_ = value; }
-  void set_h(std::uint8_t value) { h_ = value; }
-  void set_l(std::uint8_t value) { l_ = value; }
-  void set_af(std::uint16_t value) {
-    a_ = value >> 8;
-    flags_ = value & 0xFF;
-  }
-  void set_bc(std::uint16_t value) {
-    b_ = value >> 8;
-    c_ = value & 0xFF;
-  }
-  void set_de(std::uint16_t value) {
-    d_ = value >> 8;
-    e_ = value & 0xFF;
-  }
-  void set_hl(std::uint16_t value) {
-    h_ = value >> 8;
-    l_ = value & 0xFF;
-  }
-  void set_sp(std::uint16_t value) { sp_ = value; }
-  void set_pc(std::uint16_t value) { pc_ = value; }
-  void set_z_flag() { flags_ |= (1 << 7); }
-  void set_n_flag() { flags_ |= (1 << 6); }
-  void set_h_flag() { flags_ |= (1 << 5); }
-  void set_c_flag() { flags_ |= (1 << 4); }
-  void reset_z_flag() { flags_ &= ~(1 << 7); }
-  void reset_n_flag() { flags_ &= ~(1 << 6); }
-  void reset_h_flag() { flags_ &= ~(1 << 5); }
-  void reset_c_flag() { flags_ &= ~(1 << 4); }
-
- private:
-  std::uint8_t a_;
-  std::uint8_t b_;
-  std::uint8_t c_;
-  std::uint8_t d_;
-  std::uint8_t e_;
-  std::uint8_t h_;
-  std::uint8_t l_;
-  std::uint8_t flags_;
-  std::uint16_t sp_;
-  std::uint16_t pc_;
-};
-
-// 命令を表すクラス。
-// このクラスを継承したクラスで具体的な命令を表す。
-class Instruction {
- public:
-  Instruction(std::uint32_t raw_code, std::uint16_t address)
-      : raw_code_(raw_code), address_(address) {}
-  virtual ~Instruction() = default;
-
-  // ニーモニックの文字列を得る。
-  virtual std::string GetMnemonicString() = 0;
-  std::uint32_t raw_code() { return raw_code_; }
-  std::uint16_t address() { return address_; }
-  virtual unsigned length() = 0;  // 命令長（単位：バイト）
-  virtual unsigned cycles() = 0;  // 命令のサイクル数（単位：T-cycle）
-
- private:
-  std::uint32_t raw_code_;  // 生の機械語命令（リトルエンディアン）
-  std::uint16_t address_;  // 命令の配置されているアドレス
-};
 
 // CPUを表すクラス。
 // Example:
@@ -107,20 +19,138 @@ class Instruction {
 //     Cpu cpu(memory);
 //     std::uint8_t cycle = cpu.Step();
 class Cpu {
+ private:
+  // CPUのレジスタを表すクラス。
+  class Registers {
+   public:
+    std::uint8_t a() { return a_; }
+    std::uint8_t b() { return b_; }
+    std::uint8_t c() { return c_; }
+    std::uint8_t d() { return d_; }
+    std::uint8_t e() { return e_; }
+    std::uint8_t h() { return h_; }
+    std::uint8_t l() { return l_; }
+    std::uint16_t af() { return ((std::uint16_t)a_ << 8) | flags_; }
+    std::uint16_t bc() { return ((std::uint16_t)b_ << 8) | c_; }
+    std::uint16_t de() { return ((std::uint16_t)d_ << 8) | e_; }
+    std::uint16_t hl() { return ((std::uint16_t)h_ << 8) | l_; }
+    std::uint16_t sp() { return sp_; }
+    std::uint16_t pc() { return pc_; }
+    bool z_flag() { return flags_ & (1 << 7); }
+    bool n_flag() { return flags_ & (1 << 6); }
+    bool h_flag() { return flags_ & (1 << 5); }
+    bool c_flag() { return flags_ & (1 << 4); }
+    void set_a(std::uint8_t value) { a_ = value; }
+    void set_b(std::uint8_t value) { b_ = value; }
+    void set_c(std::uint8_t value) { c_ = value; }
+    void set_d(std::uint8_t value) { d_ = value; }
+    void set_e(std::uint8_t value) { e_ = value; }
+    void set_h(std::uint8_t value) { h_ = value; }
+    void set_l(std::uint8_t value) { l_ = value; }
+    void set_af(std::uint16_t value) {
+      a_ = value >> 8;
+      flags_ = value & 0xFF;
+    }
+    void set_bc(std::uint16_t value) {
+      b_ = value >> 8;
+      c_ = value & 0xFF;
+    }
+    void set_de(std::uint16_t value) {
+      d_ = value >> 8;
+      e_ = value & 0xFF;
+    }
+    void set_hl(std::uint16_t value) {
+      h_ = value >> 8;
+      l_ = value & 0xFF;
+    }
+    void set_sp(std::uint16_t value) { sp_ = value; }
+    void set_pc(std::uint16_t value) { pc_ = value; }
+    void set_z_flag() { flags_ |= (1 << 7); }
+    void set_n_flag() { flags_ |= (1 << 6); }
+    void set_h_flag() { flags_ |= (1 << 5); }
+    void set_c_flag() { flags_ |= (1 << 4); }
+    void reset_z_flag() { flags_ &= ~(1 << 7); }
+    void reset_n_flag() { flags_ &= ~(1 << 6); }
+    void reset_h_flag() { flags_ &= ~(1 << 5); }
+    void reset_c_flag() { flags_ &= ~(1 << 4); }
+
+   private:
+    std::uint8_t a_;
+    std::uint8_t b_;
+    std::uint8_t c_;
+    std::uint8_t d_;
+    std::uint8_t e_;
+    std::uint8_t h_;
+    std::uint8_t l_;
+    std::uint8_t flags_;
+    std::uint16_t sp_;
+    std::uint16_t pc_;
+  };
+
+  // 命令を表すクラス。
+  // このクラスを継承したクラスで具体的な命令を表す。
+  class Instruction {
+   public:
+    Instruction(std::vector<std::uint8_t>&& raw_code, std::uint16_t address,
+                unsigned length, unsigned mcycles)
+        : raw_code_(std::move(raw_code)),
+          address_(address),
+          length_(length),
+          mcycles_(mcycles) {}
+    virtual ~Instruction() = default;
+
+    // ニーモニックの文字列を得る。
+    virtual std::string GetMnemonicString() = 0;
+    // 命令を実行する。
+    virtual void Execute(Cpu& cpu) = 0;
+    std::vector<std::uint8_t>& raw_code() { return raw_code_; }
+    std::uint16_t address() { return address_; }
+    unsigned length() { return length_; }
+    unsigned mcycles() { return mcycles_; }
+
+   private:
+    std::vector<std::uint8_t>
+        raw_code_;  // 生の機械語命令（リトルエンディアン）
+    std::uint16_t address_;  // 命令の配置されているアドレス
+    unsigned length_;        // 命令長（単位：バイト）
+    unsigned mcycles_;       // 命令のサイクル数（単位：M-cycle）
+  };
+
+  class Nop : public Instruction {
+   public:
+    Nop(std::uint16_t address)
+        : Instruction(std::vector<std::uint8_t>{0x00}, address, 1, 1) {}
+    std::string GetMnemonicString() override;
+    void Execute(Cpu& cpu) override;
+  };
+
+  class JpU16 : public Instruction {
+   public:
+    JpU16(std::vector<std::uint8_t>&& raw_code, std::uint16_t address,
+          std::uint16_t imm)
+        : Instruction(std::move(raw_code), address, 3, 4), imm_(imm) {}
+    std::string GetMnemonicString() override;
+    void Execute(Cpu& cpu) override;
+
+   private:
+    std::uint16_t imm_;
+  };
+
  public:
   Cpu(Memory& memory) : registers_(), memory_(memory) {
     registers_.set_pc(0x100);
   }
-  // CPUを1命令分進め、経過したクロック数を返す。
+  // CPUを1命令分進め、経過したクロック数（単位：M-cycle）を返す。
   unsigned Step();
 
  private:
-  std::shared_ptr<Instruction> ExecuteInstruction();
-  std::shared_ptr<Instruction> ExecutePrefixedInstruction();
-  std::shared_ptr<Instruction> ExecuteNop();
-  std::shared_ptr<Instruction> ExecuteJpU16();
+  void Execute(std::shared_ptr<Instruction> inst) { inst->Execute(*this); }
+  std::shared_ptr<Instruction> FetchInstruction();
+  std::shared_ptr<Instruction> FetchPrefixedInstruction();
+  std::shared_ptr<Instruction> FetchNop();
+  std::shared_ptr<Instruction> FetchJpU16();
 
-  CpuRegisters registers_;
+  Registers registers_;
   Memory& memory_;
 };
 
